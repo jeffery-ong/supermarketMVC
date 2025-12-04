352595393372adea575f8a29e980bc3edfdd751e
jeffery ong <jefferyong115@gmail.com>
2025-12-05 03:50:38 +0800

    LAST ONE I GO SLEEP

--- SupermarketApp/controllers/PageController.js
+++
@@ -1,9 +0,0 @@
-const PageController = {
-    showHome: (req, res) => {
-        res.render('index', {
-            user: req.session.user
-        });
-    }
-};
-
-module.exports = PageController;
